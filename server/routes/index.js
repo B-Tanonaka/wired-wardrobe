7fd1741b64182f74dd04093846e088fd71326530
Brett Tanonaka <tano.brett@gmail.com>
2023-09-30 17:33:09 -0700

    Changed back to Atlier API

--- server/routes/index.js
+++ server/routes/index.js
@@ -10,7 +10,10 @@ module.exports.products = products;
 module.exports.questions = questions;
 module.exports.reviews = reviews;
 module.exports.interactions = router.post('/interactions', (req, res) => {
-  axios.post('http://3.137.191.193/', {
+  axios.post(`${process.env.ATLIER_API_ROUTE}/interactions?element=${req.params.element}&widget=${req.params.widget}&time=${req.params.time}`, {
+    headers: {
+      Authorization: process.env.GITHUB_API_KEY,
+    },
   })
     .then(() => {
       res.status(201).send('Content created');
@@ -19,16 +22,3 @@ module.exports.interactions = router.post('/interactions', (req, res) => {
       res.status(422).send(err);
     });
 });
-// module.exports.interactions = router.post('/interactions', (req, res) => {
-//   axios.post(`${process.env.ATLIER_API_ROUTE}/interactions?element=${req.params.element}&widget=${req.params.widget}&time=${req.params.time}`, {
-//     headers: {
-//       Authorization: process.env.GITHUB_API_KEY,
-//     },
-//   })
-//     .then(() => {
-//       res.status(201).send('Content created');
-//     })
-//     .then((err) => {
-//       res.status(422).send(err);
-//     });
-// });
